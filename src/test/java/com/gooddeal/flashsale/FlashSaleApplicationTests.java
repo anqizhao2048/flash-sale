15f5980e029cb5c4b2dd2e3830936102cb8e9843
anqizhao2048 <anqizhao1024@qq.com>
2023-11-09 21:48:54 -0800

    initiate codebase

---
+++ src/test/java/com/gooddeal/flashsale/FlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gooddeal.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
