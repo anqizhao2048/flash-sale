bd1c1cfbb6606f611f319e78dc9c4fe40607b052
anqizhao2048 <anqizhao1024@qq.com>
2023-11-10 08:07:06 -0800

    applcation properties configuration

--- src/main/java/com/gooddeal/flashsale/FlashSaleApplication.java
+++ src/main/java/com/gooddeal/flashsale/FlashSaleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.gooddeal.flashsale.db.mappers")
+@ComponentScan(basePackages = {"com.gooddeal"})
 public class FlashSaleApplication {
 
     public static void main(String[] args) {
